c6b86bfc7ca629aa894541c6800153492ec73c26
vsantos1711 <vsantos067100@gmail.com>
2023-01-03 17:44:21 -0300

    Start function login

--- src/components/FormLogin/index.js
+++ src/components/FormLogin/index.js
@@ -2,19 +2,32 @@ import * as S from "./styled";
 import { useNavigation } from "@react-navigation/native";
 import TextInput from "../TextInput";
 import Btn from "../Btn";
+import { useState } from "react";
 
-const FormLogin = ({ Title }) => {
+const handleClickCadastro = () => {
+  console.log({ user, senha });
+};
+
+const FormLogin = () => {
   const { navigate } = useNavigation();
+
+  const [user, setUser] = useState("");
+  const [password, setPassword] = useState("");
+  const [message, setMessage] = useState("");
 
   return (
     <S.Form>
       <S.Title>LOGIN</S.Title>
-      <TextInput PlaceHolder="Email" />
-      <TextInput PlaceHolder="Senha" SecureTextEntry={true} />
+      <TextInput PlaceHolder="Email" Onchange={(text) => setUser} />
+      <TextInput
+        PlaceHolder="Senha"
+        SecureTextEntry={true}
+        Onchange={(text) => setPassword}
+      />
       <Btn
         TextButton="ACESSAR"
         OnPress={() => {
-          console.log("o botao foi clicado");
+          handleClickCadastro();
         }}
       />
       <S.Text01>Não possui acesso?</S.Text01>
